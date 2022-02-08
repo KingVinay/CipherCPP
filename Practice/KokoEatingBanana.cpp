e073bff6fe4bf7137a1c6bcc18125c40860e232a
Vinay Singh <vinaysthakur15@gmail.com>
2022-02-08 15:15:39 +0530

    Cipher Practice Problems

--- Practice/KokoEatingBanana.cpp
+++ Practice/KokoEatingBanana.cpp
@@ -25,3 +25,22 @@ int minEatingSpeed(vector<int>& piles, int h) {
         
     }
 
+    // Optimised Solution (O(nlogn))
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+        int low = 1, high = *max_element(piles.begin(),piles.end()), mid = 0;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            int t = 0;
+            for (int i = 0; i < piles.size(); i ++) 
+                t += ceil(1.0 * piles[i] / mid);
+            if (t > h)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return low;
+        
+    }
+
